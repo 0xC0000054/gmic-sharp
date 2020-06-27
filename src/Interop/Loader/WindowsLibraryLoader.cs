0952e505b3c2ec16cd4e57efe2a0b7938381f3ad
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2020-06-27 08:43:10 -0600

    Disable the LoadLibrary error dialog
    LoadLibrary can show an error dialog if it cannot find a DLL
    dependency.

--- src/Interop/Loader/WindowsLibraryLoader.cs
+++ src/Interop/Loader/WindowsLibraryLoader.cs
@@ -25,7 +25,13 @@ namespace GmicSharp.Interop
 
         protected override LoadLibraryResult LoadLibrary(string path)
         {
-            IntPtr handle = NativeMethods.LoadLibraryW(path);
+            IntPtr handle = IntPtr.Zero;
+
+            // Disable the error dialog that LoadLibrary shows if it cannot find a DLL dependency.
+            using (new DisableLoadLibraryErrorDialog())
+            {
+                handle = NativeMethods.LoadLibraryW(path);
+            }
 
             if (handle != IntPtr.Zero)
             {
@@ -37,10 +43,63 @@ namespace GmicSharp.Interop
             }
         }
 
+        private static class NativeConstants
+        {
+            public const uint SEM_FAILCRITICALERRORS = 1;
+        }
+
         private static class NativeMethods
         {
             [DllImport("kernel32.dll", EntryPoint = "LoadLibraryW")]
             public static extern IntPtr LoadLibraryW([In(), MarshalAs(UnmanagedType.LPWStr)] string lpLibFileName);
+
+            [DllImport("kernel32.dll", ExactSpelling = true)]
+            public static extern uint SetErrorMode([In()] uint uMode);
+
+            [DllImport("kernel32.dll", ExactSpelling = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool SetThreadErrorMode([In()] uint dwNewMode, [Out()] out uint lpOldMode);
         }
+
+        private sealed class DisableLoadLibraryErrorDialog : IDisposable
+        {
+            private static readonly bool isWindows7OrLater = IsWindows7OrLater();
+
+            private readonly uint oldMode;
+
+            public DisableLoadLibraryErrorDialog()
+            {
+                oldMode = SetErrorMode(NativeConstants.SEM_FAILCRITICALERRORS);
+            }
+
+            private static bool IsWindows7OrLater()
+            {
+                OperatingSystem operatingSystem = Environment.OSVersion;
+
+                return operatingSystem.Platform == PlatformID.Win32NT && operatingSystem.Version >= new Version(6, 1);
+            }
+
+            private static uint SetErrorMode(uint newMode)
+            {
+                uint oldMode;
+
+                if (isWindows7OrLater)
+                {
+                    NativeMethods.SetThreadErrorMode(newMode, out oldMode);
+                }
+                else
+                {
+                    oldMode = NativeMethods.SetErrorMode(newMode);
+                }
+
+                return oldMode;
+            }
+
+            public void Dispose()
+            {
+                SetErrorMode(oldMode);
+            }
+        }
+
     }
 }
