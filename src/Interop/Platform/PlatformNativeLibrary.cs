6717182b5ebb1d1eaf6c92c519c126147d33bd64
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2021-10-09 00:45:27 -0600

    Document the Load and GetExport methods

--- src/Interop/Platform/PlatformNativeLibrary.cs
+++ src/Interop/Platform/PlatformNativeLibrary.cs
@@ -15,8 +15,23 @@ namespace GmicSharp.Interop
 {
     internal abstract class PlatformNativeLibrary
     {
+        /// <summary>
+        /// Loads a native library from the specified path.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns>
+        /// A <see cref="LoadLibraryResult"/> containing the result of the operation.
+        /// </returns>
         internal abstract LoadLibraryResult Load(string path);
 
+        /// <summary>
+        /// Gets the exported symbol with the specified name.
+        /// </summary>
+        /// <param name="libraryHandle">The library handle.</param>
+        /// <param name="name">The name of the exported symbol.</param>
+        /// <returns>
+        /// The address of the exported symbol; otherwise, <see cref="IntPtr.Zero"/> if the symbol was not found.
+        /// </returns>
         internal abstract IntPtr GetExport(IntPtr libraryHandle, string name);
 
 
