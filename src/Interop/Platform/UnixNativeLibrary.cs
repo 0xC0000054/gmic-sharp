f130e9438f8ea06fce48a529f26cb30f799ebdd8
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2021-10-09 01:40:04 -0600

    Remove the NativeLibraryHelper class
    It used reflection, which is not compatible with the assembly trimming
    that was introduced in .NET Core 3.0.

--- src/Interop/Platform/UnixNativeLibrary.cs
+++ src/Interop/Platform/UnixNativeLibrary.cs
@@ -10,7 +10,6 @@
 ////////////////////////////////////////////////////////////////////////
 
 using System;
-using System.Reflection;
 using System.Runtime.InteropServices;
 
 namespace GmicSharp.Interop
@@ -27,37 +26,23 @@ namespace GmicSharp.Interop
 
         internal sealed override LoadLibraryResult Load(string path)
         {
-            if (NativeLibraryHelper.IsSupported)
+            IntPtr handle = UnixLoadLibrary(path, NativeConstants.RTLD_NOW);
+
+            if (handle != IntPtr.Zero)
             {
-                return NativeLibraryHelper.Load(path);
+                return new LoadLibraryResult(handle);
             }
             else
             {
-                IntPtr handle = UnixLoadLibrary(path, NativeConstants.RTLD_NOW);
+                string message = Marshal.PtrToStringAnsi(UnixGetErrorMessage()) ?? string.Empty;
 
-                if (handle != IntPtr.Zero)
-                {
-                    return new LoadLibraryResult(handle);
-                }
-                else
-                {
-                    string message = Marshal.PtrToStringAnsi(UnixGetErrorMessage()) ?? string.Empty;
-
-                    return new LoadLibraryResult(new ExternalException(message));
-                }
+                return new LoadLibraryResult(new ExternalException(message));
             }
         }
 
         internal sealed override IntPtr GetExport(IntPtr libraryHandle, string name)
         {
-            if (NativeLibraryHelper.IsSupported)
-            {
-                return NativeLibraryHelper.GetExport(libraryHandle, name);
-            }
-            else
-            {
-                return UnixGetExportedSymbol(libraryHandle, name);
-            }
+            return UnixGetExportedSymbol(libraryHandle, name);
         }
 
         private IntPtr UnixLoadLibrary(string fileName, int flags)
@@ -130,82 +115,6 @@ namespace GmicSharp.Interop
 
                 [DllImport("libc", EntryPoint = "dlsym")]
                 internal static extern IntPtr GetExportedSymbol(IntPtr handle, [MarshalAs(UnmanagedType.LPStr)] string symbol);
-            }
-        }
-
-        private static class NativeLibraryHelper
-        {
-            private delegate IntPtr NativeLibraryLoad(string path);
-            private delegate bool NativeLibraryTryGetExport(IntPtr handle, string symbolName, out IntPtr address);
-
-            private static readonly NativeLibraryLoad load;
-            private static readonly NativeLibraryTryGetExport tryGetExport;
-
-            static NativeLibraryHelper()
-            {
-                // The NativeLibrary class was introduced in .NET Core 3.0.
-                // We use it via reflection so that we can target .NET Standard 2.1.
-
-                Type nativeLibraryType = Type.GetType("System.Runtime.InteropServices.NativeLibrary");
-
-                if (nativeLibraryType != null)
-                {
-                    MethodInfo loadMethodInfo = nativeLibraryType.GetMethod("Load",
-                                                                            BindingFlags.Public | BindingFlags.Static,
-                                                                            null,
-                                                                            CallingConventions.Any,
-                                                                            new Type[] { typeof(string) },
-                                                                            null);
-
-                    if (loadMethodInfo != null)
-                    {
-                        load = (NativeLibraryLoad)Delegate.CreateDelegate(typeof(NativeLibraryLoad), loadMethodInfo);
-                    }
-
-                    MethodInfo tryGetExportMethodInfo = nativeLibraryType.GetMethod("TryGetExport",
-                                                                                    BindingFlags.Public | BindingFlags.Static,
-                                                                                    null,
-                                                                                    CallingConventions.Any,
-                                                                                    new Type[] { typeof(IntPtr), typeof(string), typeof(IntPtr).MakeByRefType() },
-                                                                                    null);
-
-                    if (tryGetExportMethodInfo != null)
-                    {
-                        tryGetExport = (NativeLibraryTryGetExport)Delegate.CreateDelegate(typeof(NativeLibraryTryGetExport), tryGetExportMethodInfo);
-                    }
-                }
-
-                IsSupported = nativeLibraryType != null && load != null && tryGetExport != null;
-            }
-
-            public static bool IsSupported { get; }
-
-            public static LoadLibraryResult Load(string path)
-            {
-                if (!IsSupported)
-                {
-                    ExceptionUtil.ThrowInvalidOperationException("The NativeLibary class is not supported by the current runtime.");
-                }
-
-                try
-                {
-                    IntPtr handle = load.Invoke(path);
-                    return new LoadLibraryResult(handle);
-                }
-                catch (Exception ex)
-                {
-                    return new LoadLibraryResult(ex);
-                }
-            }
-
-            public static IntPtr GetExport(IntPtr handle, string symbolName)
-            {
-                if (!IsSupported)
-                {
-                    ExceptionUtil.ThrowInvalidOperationException("The NativeLibary class is not supported by the current runtime.");
-                }
-
-                return tryGetExport.Invoke(handle, symbolName, out IntPtr address) ? address : IntPtr.Zero;
             }
         }
     }
